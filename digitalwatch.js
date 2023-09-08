ab24d891982dd214dcbb229cfcb1ae910b69f0bc
SOWMIYA M <125813144+Sowmiya-Mani@users.noreply.github.com>
2023-09-08 11:06:09 +0530

    Update digitalwatch.js

--- digitalwatch.js
+++ digitalwatch.js
@@ -1,20 +1,10 @@
-let ampm=document.getElementById('ampm');
-function displaytime(){
-    let datetime = new Date();
-    let hr = datetime.getHours();
-    let min = zeropad(datetime.getMinutes());
-    let sec = zeropad(datetime.getSeconds());
-     if(hr>=12){
-      hr=hr-12;
-      ampm.innerHTML = 'PM';
-     }
-    document.getElementById('hours').innerHTML =zeropad(hr)
-    document.getElementById('minutes').innerHTML = min
-    document.getElementById('seconds').innerHTML = sec
-}
 
-function zeropad(num){
-    return num<10? '0'+num : num
-}
-
-setInterval(displaytime,500)
+        function convertor(){
+            const num=Number(document.getElementById('celsius').value)
+            const fahrenheit = (num * 1.8)+32
+            const result=document.getElementById("result")
+            result.innerHTML= fahrenheit + "'F"
+        }
+         
+    
+    
